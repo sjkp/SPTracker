f00873105ab7e6bbd5b7df445ab90eb121cee91c
Simon Pedersen <mail@sjkp.dk>
2014-11-13 21:14:58 +0100

    SP project

---
+++ SPTracker/Features/Feature1/Feature1.feature
@@ -0,0 +1,9 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="490f8e03-34a0-477c-a766-dcecf9bfca64" description="SharePoint App Feature" featureId="490f8e03-34a0-477c-a766-dcecf9bfca64" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="SPTracker Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="5c59f862-0185-4f52-9005-3d552f864acc" />
+    <projectItemReference itemId="59d304fc-5e63-4d34-8a43-eb320652e6bf" />
+    <projectItemReference itemId="6543ce5e-b5ad-4408-9769-5797e08f6908" />
+    <projectItemReference itemId="8f2a3282-d9eb-4a90-8162-ced566b62918" />
+  </projectItems>
+</feature>
---
+++ SPTracker/Scripts/App.js
@@ -0,0 +1,57 @@
+﻿'use strict';
+
+var context = SP.ClientContext.get_current();
+var user = context.get_web().get_currentUser();
+
+// This code runs when the DOM is ready and creates a context object which is needed to use the SharePoint object model
+$(document).ready(function () {
+    getUserName();
+    SP.SOD.executeFunc('sp.js', 'SP.ClientContext', function () {
+
+        var appContext = SP.ClientContext.get_current();
+
+        var appWeb = appContext.get_web();
+        var hostWebUrl = decodeURIComponent(getQueryStringParameter("SPHostUrl"));
+        var appWebUrl = decodeURIComponent(getQueryStringParameter("SPAppWebUrl"));
+
+        var hostWebContext=new SP.AppContextSite(appContext,hostWebUrl);
+
+        var web = hostWebContext.get_site().get_rootWeb();
+        appContext.load(web);
+
+        appContext.executeQueryAsync(function () {
+            $('#hostweb').text(web.get_title());
+        },
+        function (err) {
+            console.log(err)
+        });
+
+    });
+});
+
+function getQueryStringParameter(urlParameterKey) {
+    var params = document.URL.split('?')[1].split('&');
+    var strParams = '';
+    for (var i = 0; i < params.length; i = i + 1) {
+        var singleParam = params[i].split('=');
+        if (singleParam[0] == urlParameterKey)
+            return decodeURIComponent(singleParam[1]);
+    }
+}
+
+// This function prepares, loads, and then executes a SharePoint query to get the current users information
+function getUserName() {
+    context.load(user);
+    context.executeQueryAsync(onGetUserNameSuccess, onGetUserNameFail);
+}
+
+// This function is executed if the above call is successful
+// It replaces the contents of the 'message' element with the user name
+function onGetUserNameSuccess() {
+    $('#message').text('Hello ' + user.get_title());
+}
+
+// This function is executed if the above call fails
+function onGetUserNameFail(sender, args) {
+    alert('Failed to get user name. Error:' + args.get_message());
+}
